2725323a840e896558494cee8a52e8dfde0529ad
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2022-10-27 13:17:12 +0200

    Created accessibilities-addon as place holder for lgln-tool and added it to test_portal

---
+++ addons/dvan/accessibilities/store/state.js
@@ -0,0 +1,14 @@
+const state = {
+    // mandatory
+    active: false,
+    id: "Accessibilities",
+    // mandatory defaults for config.json parameters
+    name: "Erreichbarkeiten",
+    icon: "bi-geo",
+    renderToWindow: false,
+    resizableWindow: true,
+    isVisibleInMenu: true,
+    deactivateGFI: false
+};
+
+export default state;
