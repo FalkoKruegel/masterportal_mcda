47bf176926f413cdd3973a01621770018406d8fd
ttpr0 <ttpro@gmx.de>
2023-01-09 22:03:16 +0100

    added bootstrap checkbox component

--- addons/dvan/decision_support/components/steps/SelectedInfrastructure.vue
+++ addons/dvan/decision_support/components/steps/SelectedInfrastructure.vue
@@ -1,10 +1,12 @@
 <script>
+import BootstrapCheckbox from "../../../share_components/BootstrapCheckbox.vue";
 import {mapGetters, mapActions, mapMutations} from "vuex";
 import getters from "../../store/getters";
 
 export default {
     name: "SelectedInfrastructure",
     components: {
+        BootstrapCheckbox
     },
     data () {
         return {
@@ -56,54 +58,22 @@ export default {
                 <div
                     class="accordion-body"
                 >
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_1_1"
-                            v-model="stepThree.local_supply.supermarket"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_1_1"
-                        >
-                            Supermärkte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_1_2"
-                            v-model="stepThree.local_supply.discounter"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_1_2"
-                        >
-                            Discounter
-                        </label>
-                    </div>
-                    <div
-                        class="form-check"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-html="true"
-                        title="Hierzu zählen Standorte des Lebensmittelhandwerkes<br> (Bäcker/Fleischer), Hof- und Bioläden und sonstige<br> Lebensmittelgeschäfte."
-                    >
-                        <input
-                            id="checkbox_3_1_3"
-                            v-model="stepThree.local_supply.others"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_1_3"
-                        >
-                            sonstige Lebensmittelgeschäfte
-                        </label>
-                    </div>
+                    <BootstrapCheckbox
+                        id="checkbox_3_1_1"
+                        v-model="stepThree.local_supply.supermarket"
+                        text="Supermärkte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_1_2"
+                        v-model="stepThree.local_supply.discounter"
+                        text="Discounter"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_1_3"
+                        v-model="stepThree.local_supply.others"
+                        text="sonstige Lebensmittelgeschäfte"
+                        tooltipText="Hierzu zählen Standorte des Lebensmittelhandwerkes<br> (Bäcker/Fleischer), Hof- und Bioläden und sonstige<br> Lebensmittelgeschäfte."
+                    />
                 </div>
             </div>
         </div>
@@ -134,174 +104,66 @@ export default {
                 <div
                     class="accordion-body"
                 >
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_1"
-                            v-model="stepThree.health.pharmacies"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_1"
-                        >
-                            Apotheken
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_2"
-                            v-model="stepThree.health.clinics"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_2"
-                        >
-                            Hochschulkliniken und Plankrankenhäuser
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_3"
-                            v-model="stepThree.health.general_physicians"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_3"
-                        >
-                            Hausärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_4"
-                            v-model="stepThree.health.paediatricians"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_4"
-                        >
-                            Kinder- und Jugendärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_5"
-                            v-model="stepThree.health.ophthalmologists"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_5"
-                        >
-                            Augenärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_6"
-                            v-model="stepThree.health.surgeons"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_6"
-                        >
-                            Chirurgen und Orthopäden
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_7"
-                            v-model="stepThree.health.gynaecologists"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_7"
-                        >
-                            Frauenärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_8"
-                            v-model="stepThree.health.dermatologists"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_8"
-                        >
-                            Hautärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_9"
-                            v-model="stepThree.health.otolaryngologist"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_9"
-                        >
-                            HNO-Arzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_10"
-                            v-model="stepThree.health.neurologist"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_10"
-                        >
-                            Nervenärzte
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_11"
-                            v-model="stepThree.health.psychotherapists"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_11"
-                        >
-                            Psychotherapeuten
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input
-                            id="checkbox_3_2_12"
-                            v-model="stepThree.health.urologists"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_2_12"
-                        >
-                            Urologen
-                        </label>
-                    </div>
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_1"
+                        v-model="stepThree.health.pharmacies"
+                        text="Apotheken"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_2"
+                        v-model="stepThree.health.clinics"
+                        text="Hochschulkliniken und Plankrankenhäuser"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_3"
+                        v-model="stepThree.health.general_physicians"
+                        text="Hausärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_4"
+                        v-model="stepThree.health.paediatricians"
+                        text="Kinder- und Jugendärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_5"
+                        v-model="stepThree.health.ophthalmologists"
+                        text="Augenärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_6"
+                        v-model="stepThree.health.surgeons"
+                        text="Chirurgen und Orthopäden"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_7"
+                        v-model="stepThree.health.gynaecologists"
+                        text="Frauenärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_8"
+                        v-model="stepThree.health.dermatologists"
+                        text="Hautärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_9"
+                        v-model="stepThree.health.otolaryngologist"
+                        text="HNO-Ärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_10"
+                        v-model="stepThree.health.neurologist"
+                        text="Nervenärzte"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_11"
+                        v-model="stepThree.health.psychotherapists"
+                        text="Psychotherapeuten"
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_2_12"
+                        v-model="stepThree.health.urologists"
+                        text="Urologen"
+                    />
                 </div>
             </div>
         </div>
@@ -332,86 +194,30 @@ export default {
                 <div
                     class="accordion-body"
                 >
-                    <div
-                        class="form-check"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-html="true"
-                        title="Einrichtungen zur Betreuung von Kindern  bis zum sechsten Lebensjahr. Ohne Horteinrichtungen."
-                    >
-                        <input
-                            id="checkbox_3_3_1"
-                            v-model="stepThree.education.nurseries"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_3_1"
-                        >
-                            Kindertagesstätten
-                        </label>
-                    </div>
-                    <div
-                        class="form-check"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-html="true"
-                        title="Primärbereich umfasst die 1. bis 4. Schuljahrgänge."
-                    >
-                        <input
-                            id="checkbox_3_3_2"
-                            v-model="stepThree.education.primary_schools"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_3_2"
-                        >
-                            Primärschulen
-                        </label>
-                    </div>
-                    <div
-                        class="form-check"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-html="true"
-                        title="Haupt-, Real-, Ober- und Gesamtschulen ohne Möglichkeit zum Erwerb der (Fach)Hochschulreife."
-                    >
-                        <input
-                            id="checkbox_3_3_3"
-                            v-model="stepThree.education.secondary_1"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_3_3"
-                        >
-                            Sekundarstufe Bereich 1, ohne (Fach)Hochschulreife
-                        </label>
-                    </div>
-                    <div
-                        class="form-check"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-html="true"
-                        title="Gesamtschulen und Gymnasien mit Möglichkeiten zum Erwerb der (Fach)Hochschulreife"
-                    >
-                        <input
-                            id="checkbox_3_3_4"
-                            v-model="stepThree.education.secondary_2"
-                            class="form-check-input"
-                            type="checkbox"
-                        >
-                        <label
-                            class="form-check-label"
-                            for="checkbox_3_3_4"
-                        >
-                            Sekundarstufe Bereich 1 und 2, mit Möglichkeit zu Erwerb der (Fach)Hochschulreife
-                        </label>
-                    </div>
+                    <BootstrapCheckbox
+                        id="checkbox_3_3_1"
+                        v-model="stepThree.education.nurseries"
+                        text="Kindertagesstätten"
+                        tooltipText="Einrichtungen zur Betreuung von Kindern  bis zum sechsten Lebensjahr. Ohne Horteinrichtungen."
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_3_2"
+                        v-model="stepThree.education.primary_schools"
+                        text="Primärschulen"
+                        tooltipText="Primärbereich umfasst die 1. bis 4. Schuljahrgänge."
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_3_3"
+                        v-model="stepThree.education.secondary_1"
+                        text="Sekundarstufe Bereich 1, ohne (Fach)Hochschulreife"
+                        tooltipText="Haupt-, Real-, Ober- und Gesamtschulen ohne Möglichkeit zum Erwerb der (Fach)Hochschulreife."
+                    />
+                    <BootstrapCheckbox
+                        id="checkbox_3_3_4"
+                        v-model="stepThree.education.secondary_2"
+                        text="Sekundarstufe Bereich 1 und 2, mit Möglichkeit zu Erwerb der (Fach)Hochschulreife"
+                        tooltipText="Gesamtschulen und Gymnasien mit Möglichkeiten zum Erwerb der (Fach)Hochschulreife."
+                    />
                     <br>
                     <p>Die niedersächsischen Schulstrukturen und die Schulform lassen sich unter folgendem Link abrufen: [Link]</p>
                 </div>
