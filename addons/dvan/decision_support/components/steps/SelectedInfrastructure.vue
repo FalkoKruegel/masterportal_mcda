22d46edc0ef096ae768d285944a7a750ee7ec403
ttpr0 <ttpro@gmx.de>
2023-01-10 10:05:48 +0100

    fix id namings

--- addons/dvan/decision_support/components/steps/SelectedInfrastructure.vue
+++ addons/dvan/decision_support/components/steps/SelectedInfrastructure.vue
@@ -34,26 +34,26 @@ export default {
     <div>
         Bitte wählen Sie Infrastrukturen, die Sie in die Analyse einbeziehen<br> wollen:
         <BootstrapAccordion
-            id="selected_infrastructures"
+            id="Accordion_3"
             body-padding-y="5px"
         >
             <!-- Nahversorgungs Infrastrukturen -->
             <BootstrapAccordionItem
-                id="collapseOne"
+                id="Accordion_3_1"
                 text="Nahversorgung"
             >
                 <BootstrapCheckbox
-                    id="checkbox_3_1_1"
+                    id="Checkbox_3_1_1"
                     v-model="stepThree.local_supply.supermarket"
                     text="Supermärkte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_1_2"
+                    id="Checkbox_3_1_2"
                     v-model="stepThree.local_supply.discounter"
                     text="Discounter"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_1_3"
+                    id="Checkbox_3_1_3"
                     v-model="stepThree.local_supply.others"
                     text="sonstige Lebensmittelgeschäfte"
                     tooltip-text="Hierzu zählen Standorte des Lebensmittelhandwerkes<br> (Bäcker/Fleischer), Hof- und Bioläden und sonstige<br> Lebensmittelgeschäfte."
@@ -62,66 +62,66 @@ export default {
 
             <!-- Gesundheits Infrastrukturen -->
             <BootstrapAccordionItem
-                id="collapseTwo"
+                id="Accordion_3_2"
                 text="Gesundheit"
             >
                 <BootstrapCheckbox
-                    id="checkbox_3_2_1"
+                    id="Checkbox_3_2_1"
                     v-model="stepThree.health.pharmacies"
                     text="Apotheken"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_2"
+                    id="Checkbox_3_2_2"
                     v-model="stepThree.health.clinics"
                     text="Hochschulkliniken und Plankrankenhäuser"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_3"
+                    id="Checkbox_3_2_3"
                     v-model="stepThree.health.general_physicians"
                     text="Hausärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_4"
+                    id="Checkbox_3_2_4"
                     v-model="stepThree.health.paediatricians"
                     text="Kinder- und Jugendärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_5"
+                    id="Checkbox_3_2_5"
                     v-model="stepThree.health.ophthalmologists"
                     text="Augenärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_6"
+                    id="Checkbox_3_2_6"
                     v-model="stepThree.health.surgeons"
                     text="Chirurgen und Orthopäden"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_7"
+                    id="Checkbox_3_2_7"
                     v-model="stepThree.health.gynaecologists"
                     text="Frauenärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_8"
+                    id="Checkbox_3_2_8"
                     v-model="stepThree.health.dermatologists"
                     text="Hautärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_9"
+                    id="Checkbox_3_2_9"
                     v-model="stepThree.health.otolaryngologist"
                     text="HNO-Ärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_10"
+                    id="Checkbox_3_2_10"
                     v-model="stepThree.health.neurologist"
                     text="Nervenärzte"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_11"
+                    id="Checkbox_3_2_11"
                     v-model="stepThree.health.psychotherapists"
                     text="Psychotherapeuten"
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_2_12"
+                    id="Checkbox_3_2_12"
                     v-model="stepThree.health.urologists"
                     text="Urologen"
                 />
@@ -129,34 +129,37 @@ export default {
 
             <!-- Bildungs Infrastrukturen -->
             <BootstrapAccordionItem
-                id="collapseThree"
+                id="Accordion_3_3"
                 text="Bildung"
             >
                 <BootstrapCheckbox
-                    id="checkbox_3_3_1"
+                    id="Checkbox_3_3_1"
                     v-model="stepThree.education.nurseries"
                     text="Kindertagesstätten"
                     tooltip-text="Einrichtungen zur Betreuung von Kindern  bis zum sechsten Lebensjahr. Ohne Horteinrichtungen."
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_3_2"
+                    id="Checkbox_3_3_2"
                     v-model="stepThree.education.primary_schools"
                     text="Primärschulen"
                     tooltip-text="Primärbereich umfasst die 1. bis 4. Schuljahrgänge."
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_3_3"
+                    id="Checkbox_3_3_3"
                     v-model="stepThree.education.secondary_1"
                     text="Sekundarstufe Bereich 1, ohne (Fach)Hochschulreife"
                     tooltip-text="Haupt-, Real-, Ober- und Gesamtschulen ohne Möglichkeit zum Erwerb der (Fach)Hochschulreife."
                 />
                 <BootstrapCheckbox
-                    id="checkbox_3_3_4"
+                    id="Checkbox_3_3_4"
                     v-model="stepThree.education.secondary_2"
                     text="Sekundarstufe Bereich 1 und 2, mit Möglichkeit zu Erwerb der (Fach)Hochschulreife"
                     tooltip-text="Gesamtschulen und Gymnasien mit Möglichkeiten zum Erwerb der (Fach)Hochschulreife."
                 />
-                <div class="callout">
+                <div
+                    id="Infotext_3_1"
+                    class="callout"
+                >
                     Die niedersächsischen Schulstrukturen und die Schulform lassen sich unter folgendem Link abrufen: [Link]
                 </div>
             </BootstrapAccordionItem>
