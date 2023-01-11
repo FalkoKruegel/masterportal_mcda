ec703c295ebe429d3aeed44a29d58b9173532fc1
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-01-11 16:59:06 +0100

    Added two checkboxes and created variables for them in state.

--- addons/dvan/decision_support/components/steps/ChooseStudyArea.vue
+++ addons/dvan/decision_support/components/steps/ChooseStudyArea.vue
@@ -1,11 +1,19 @@
 <script>
-import {mapActions, mapMutations} from "vuex";
+import {mapGetters, mapActions, mapMutations} from "vuex";
+import BootstrapCheckbox from "../../../share_components/BootstrapCheckbox.vue";
+import getters from "../../store/getters";
 
 export default {
     name: "ChooseStudyArea",
+    components: {
+        BootstrapCheckbox
+    },
     data () {
         return {
         };
+    },
+    computed: {
+        ...mapGetters("Tools/DecisionSupport", Object.keys(getters)),
     },
     methods: {
         ...mapActions("Tools/DecisionSupport", [
@@ -23,5 +31,15 @@ export default {
         <p>
             Bitte wählen Sie ein Analysegebiet aus einer der Listen.
         </p>
+        <BootstrapCheckbox
+            id="Checkbox_2_1"
+            v-model="stepTwo.wholeLowerSaxony"
+            text="Niedersachsenweite Analyse"
+        />
+        <BootstrapCheckbox
+            id="Checkbox_2_2"
+            v-model="stepTwo.ownArea"
+            text="Eigene Gebietsselektion"
+        />
     </div>
 </template>
