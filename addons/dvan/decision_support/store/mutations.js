bb143a1c6766bd5389c35ed886673b36b74017cb
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-02-07 12:03:25 +0100

    added some setters and corresponding actions to change values of state-variables for step 5 in a logical manner. Not tested yet.

--- addons/dvan/decision_support/store/mutations.js
+++ addons/dvan/decision_support/store/mutations.js
@@ -8,7 +8,38 @@ const mutations = {
      * {setKey:   (state, payload) => *   state[key] = payload * }
      * will be returned.
      */
-    ...generateSimpleMutations(state)
+    ...generateSimpleMutations(state),
+    // setters for the different time zones in step 5
+    SET_VERY_GOOD (category, infrastructure, value) {
+        if (value >= state.stepFive[category][infrastructure].good) {
+            state.stepFive[category][infrastructure].good = value + 1;
+        }
+        state.stepFive[infrastructure].very_good = value;
+    },
+    SET_GOOD (category, infrastructure, value) {
+        if (value >= state.stepFive[category][infrastructure].sufficient) {
+            state.stepFive[category][infrastructure].sufficient = value + 1;
+        }
+        if (value <= state.stepFive[category][infrastructure].very_good) {
+            state.stepFive[category][infrastructure].very_good = value - 1;
+        }
+        state.stepFive[category][infrastructure].good = value;
+    },
+    SET_SUFFICIENT (category, infrastructure, value) {
+        if (value >= state.stepFive[category][infrastructure].deficient) {
+            state.stepFive[category][infrastructure].deficient = value + 1;
+        }
+        if (value <= state.stepFive[category][infrastructure].good) {
+            state.stepFive[category][infrastructure].good = value - 1;
+        }
+        state.stepFive[category][infrastructure].sufficient = value;
+    },
+    SET_DEFICIENT (category, infrastructure, value) {
+        if (value <= state.stepFive[category][infrastructure].sufficient) {
+            state.stepFive[category][infrastructure].sufficient = value - 1;
+        }
+        state.stepFive[category][infrastructure].deficient = value;
+    }
 
 
 };
