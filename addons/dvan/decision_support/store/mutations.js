ab2e49897924af81a413296a9b5c524c2d1ad5fe
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-03-03 10:18:51 +0100

    Reworked setters for properties of step 5 and added some comments

--- addons/dvan/decision_support/store/mutations.js
+++ addons/dvan/decision_support/store/mutations.js
@@ -10,35 +10,45 @@ const mutations = {
      */
     ...generateSimpleMutations(state),
     // setters for the different time zones in step 5
-    SET_VERY_GOOD (category, infrastructure, value) {
-        if (value >= state.stepFive[category][infrastructure].good) {
-            state.stepFive[category][infrastructure].good = value + 1;
+    // folowing mutations updates the very good property in a specified category and infrastructure of the state
+    // it also ensures that the corresponding good-property will be updated if it is smaller than the new very good property
+    SET_VERY_GOOD (currentState, category, infrastructure, value) {
+        if (value >= currentState.stepFive[category][infrastructure].good) {
+            currentState.stepFive[category][infrastructure].good = value + 1;
         }
-        state.stepFive[infrastructure].very_good = value;
+        currentState.stepFive[category][infrastructure].very_good = value;
     },
-    SET_GOOD (category, infrastructure, value) {
-        if (value >= state.stepFive[category][infrastructure].sufficient) {
-            state.stepFive[category][infrastructure].sufficient = value + 1;
+    // folowing mutations updates the good property in a specified category and infrastructure of the state
+    // it also ensures that the corresponding sufficient-property will be updated if it is smaller than the new good property
+    // it also ensures that the corresponding very good-property will be updated if it is bigger than the new good property
+    SET_GOOD (currentState, category, infrastructure, value) {
+        if (value >= currentState.stepFive[category][infrastructure].sufficient) {
+            currentState.stepFive[category][infrastructure].sufficient = value + 1;
         }
-        if (value <= state.stepFive[category][infrastructure].very_good) {
-            state.stepFive[category][infrastructure].very_good = value - 1;
+        if (value <= currentState.stepFive[category][infrastructure].very_good) {
+            currentState.stepFive[category][infrastructure].very_good = value - 1;
         }
-        state.stepFive[category][infrastructure].good = value;
+        currentState.stepFive[category][infrastructure].good = value;
     },
-    SET_SUFFICIENT (category, infrastructure, value) {
-        if (value >= state.stepFive[category][infrastructure].deficient) {
-            state.stepFive[category][infrastructure].deficient = value + 1;
+    // folowing mutations updates the sufficient property in a specified category and infrastructure of the state
+    // it also ensures that the corresponding deficient-property will be updated if it is smaller than the new sufficient property
+    // it also ensures that the corresponding good-property will be updated if it is bigger than the new sufficient property
+    SET_SUFFICIENT (currentState, category, infrastructure, value) {
+        if (value >= currentState.stepFive[category][infrastructure].deficient) {
+            currentState.stepFive[category][infrastructure].deficient = value + 1;
         }
-        if (value <= state.stepFive[category][infrastructure].good) {
-            state.stepFive[category][infrastructure].good = value - 1;
+        if (value <= currentState.stepFive[category][infrastructure].good) {
+            currentState.stepFive[category][infrastructure].good = value - 1;
         }
-        state.stepFive[category][infrastructure].sufficient = value;
+        currentState.stepFive[category][infrastructure].sufficient = value;
     },
-    SET_DEFICIENT (category, infrastructure, value) {
-        if (value <= state.stepFive[category][infrastructure].sufficient) {
-            state.stepFive[category][infrastructure].sufficient = value - 1;
+    // folowing mutations updates the deficient property in a specified category and infrastructure of the state
+    // it also ensures that the corresponding sufficent-property will be updated if it is bigger than the new deficient property
+    SET_DEFICIENT (currentState, category, infrastructure, value) {
+        if (value <= currentState.stepFive[category][infrastructure].sufficient) {
+            currentState.stepFive[category][infrastructure].sufficient = value - 1;
         }
-        state.stepFive[category][infrastructure].deficient = value;
+        currentState.stepFive[category][infrastructure].deficient = value;
     }
 
 
