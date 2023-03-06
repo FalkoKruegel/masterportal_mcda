7117c697312f01587a7705e0a41b1cb4b4df5451
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-03-06 11:10:04 +0100

    removed old and unnecessary actions

--- addons/dvan/decision_support/store/actions.js
+++ addons/dvan/decision_support/store/actions.js
@@ -1,21 +1,6 @@
 const actions = {
     initialize () {
         return 1;
-    },
-    updateVeryGood ({commit}, category, infrastructure, value) {
-        commit("SET_VERY_GOOD", category, infrastructure, value);
-    },
-    updateGood ({commit}, category, infrastructure, value) {
-        commit("SET_GOOD", category, infrastructure, value);
-
-    },
-    updateSufficient ({commit}, category, infrastructure, value) {
-        commit("SET_Sufficient", category, infrastructure, value);
-
-    },
-    updateDeficient ({commit}, category, infrastructure, value) {
-        commit("SET_Deficient", category, infrastructure, value);
-
     }
 };
 
