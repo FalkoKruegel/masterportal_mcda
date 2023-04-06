9b063c1453eebaf16066bd6e303e76049fe40382
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-04-06 09:54:37 +0200

    setup basic accordion just like in decisionsupport tool. Copied accordion item component and footer component to shared component folder because they are used by two tools now.

--- addons/dvan/spatial_access/components/SpatialAccess.vue
+++ addons/dvan/spatial_access/components/SpatialAccess.vue
@@ -1,12 +1,28 @@
 <script>
 import ToolTemplate from "/src/modules/tools/ToolTemplate.vue";
+import AccordionItem from "../../share_components/accordion/AccordionItem.vue";
+import AccordionFooter from "../../share_components/accordion/AccordionFooter.vue";
 import {mapGetters, mapActions, mapMutations} from "vuex";
 import getters from "../store/getters";
 
 export default {
     name: "SpatialAccess",
     components: {
-        ToolTemplate
+        ToolTemplate,
+        AccordionItem,
+        AccordionFooter
+    },
+    data () {
+        return {
+            steps: {
+                0: true,
+                1: false,
+                2: false,
+                3: false,
+                4: false,
+                5: false
+            }
+        };
     },
     computed: {
         ...mapGetters("Tools/SpatialAccess", Object.keys(getters))
@@ -47,6 +63,20 @@ export default {
             if (model) {
                 model.set("isActive", false);
             }
+        },
+        openStep (index) {
+            if (this.steps[index] === true) {
+                this.steps[index] = false;
+                return;
+            }
+            for (const i in this.steps) {
+                this.steps[i] = false;
+            }
+            this.steps[index] = true;
+
+            if (index === 6) {
+                this.stepSeven.status = "unchanged";
+            }
         }
     }
 };
@@ -60,18 +90,86 @@ export default {
         :render-to-window="renderToWindow"
         :resizable-window="resizableWindow"
         :deactivate-gfi="deactivateGFI"
+        :initial-width="450"
     >
         <template #toolBody>
             <div
-                v-if="active"
-                id="vue-addon"
+                id="decisionSupportAccordion"
+                class="accordion accordion-flush full-window"
             >
-                {{ "Hier wird das Tool räumlicher Zugang entstehen." }}
+                <AccordionItem
+                    title="Schritt 1: Analyse starten oder bestehende Analyse laden"
+                    status="valid"
+                    :opened="steps[0]"
+                    @click="openStep(0)"
+                />
+                <AccordionItem
+                    title="Schritt 2: Facharztgruppe und Planungsbereich auswählen"
+                    status="invalid"
+                    :opened="steps[1]"
+                    @click="openStep(1)"
+                >
+                    <AccordionFooter
+                        @forwardClick="openStep(2)"
+                        @backClick="openStep(0)"
+                    />
+                </AccordionItem>
+                <AccordionItem
+                    title="Schritt 3: Ärztlichen Teilnahmeumfang und Kapazitäten definieren"
+                    status="invalid"
+                    :opened="steps[2]"
+                    @click="openStep(2)"
+                >
+                    <AccordionFooter
+                        @forwardClick="openStep(3)"
+                        @backClick="openStep(1)"
+                    />
+                </AccordionItem>
+                <AccordionItem
+                    title="Schritt 4: Altersgruppe der Bevölkerung auswählen"
+                    status="invalid"
+                    :opened="steps[3]"
+                    @click="openStep(3)"
+                >
+                    <AccordionFooter
+                        @backClick="openStep(2)"
+                        @forwardClick="openStep(4)"
+                    />
+                </AccordionItem>
+                <AccordionItem
+                    title="Schritt 5: Transportmittel und Entfernungsabgwichtung wählen"
+                    status="invalid"
+                    :opened="steps[4]"
+                    @click="openStep(4)"
+                >
+                    <AccordionFooter
+                        @backClick="openStep(3)"
+                        @forwardClick="openStep(5)"
+                    />
+                </AccordionItem>
+                <AccordionItem
+                    title="Schritt 6: Zusammenfassung und Ergebnisse"
+                    status="invalid"
+                    :opened="steps[5]"
+                    @click="openStep(5)"
+                >
+                    <AccordionFooter
+                        @backClick="openStep(4)"
+                        @forwardClick="openStep(6)"
+                    />
+                </AccordionItem>
             </div>
         </template>
     </ToolTemplate>
 </template>
 
 <style lang="scss" scoped>
+.accordion {
+    --bs-accordion-btn-padding-x: 10px;
+    --bs-accordion-btn-padding-y: 10px;
+}
 
+.full-window {
+    margin: -1.25rem;
+}
 </style>
