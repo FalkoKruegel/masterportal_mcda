93b37341ac730e347e24043f4c7892d555abb58e
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-04-06 09:20:14 +0200

    Added folder and basic files for new addon and registered it in test_portal

---
+++ addons/dvan/spatial_access/components/SpatialAccess.vue
@@ -0,0 +1,77 @@
+<script>
+import ToolTemplate from "/src/modules/tools/ToolTemplate.vue";
+import {mapGetters, mapActions, mapMutations} from "vuex";
+import getters from "../store/getters";
+
+export default {
+    name: "SpatialAccess",
+    components: {
+        ToolTemplate
+    },
+    computed: {
+        ...mapGetters("Tools/SpatialAccess", Object.keys(getters))
+    },
+    created () {
+        this.$on("close", this.close);
+    },
+    /**
+     * Put initialize here if mounting occurs after config parsing
+     * @returns {void}
+     */
+    mounted () {
+        this.initialize();
+        if (this.isActive) {
+            this.setActive(true);
+        }
+    },
+    methods: {
+        ...mapActions("Tools/SpatialAccess", [
+            "initialize"
+        ]),
+        ...mapMutations("Tools/SpatialAccess", [
+            "setActive"
+        ]),
+
+        /**
+         * Closes this tool window by setting active to false
+         * @returns {void}
+         */
+        close () {
+            this.setActive(false);
+
+            // TODO replace trigger when Menu is migrated
+            // set the backbone model to active false for changing CSS class in menu (menu/desktop/tool/view.toggleIsActiveClass)
+            // else the menu-entry for this tool is always highlighted
+            const model = Radio.request("ModelList", "getModelByAttributes", {id: this.$store.state.Tools.SpatialAccess.id});
+
+            if (model) {
+                model.set("isActive", false);
+            }
+        }
+    }
+};
+</script>
+
+<template lang="html">
+    <ToolTemplate
+        :title="'Räumlicher Zugang'"
+        :icon="icon"
+        :active="active"
+        :render-to-window="renderToWindow"
+        :resizable-window="resizableWindow"
+        :deactivate-gfi="deactivateGFI"
+    >
+        <template #toolBody>
+            <div
+                v-if="active"
+                id="vue-addon"
+            >
+                {{ "Hier wird das Tool räumlicher Zugang entstehen." }}
+            </div>
+        </template>
+    </ToolTemplate>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ addons/dvan/spatial_access/store/state.js
@@ -0,0 +1,14 @@
+const state = {
+    // mandatory
+    active: false,
+    id: "SpatialAccess",
+    // mandatory defaults for config.json parameters
+    name: "Räumlicher Zugang",
+    icon: "bi-geo",
+    renderToWindow: false,
+    resizableWindow: true,
+    isVisibleInMenu: true,
+    deactivateGFI: false
+};
+
+export default state;
