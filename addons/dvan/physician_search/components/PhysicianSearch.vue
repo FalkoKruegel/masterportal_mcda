07a03652d1e548707c2a38f877bb7e45e0e6c010
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-05-05 08:53:38 +0200

    Setup needed files for new component and changed paths and names where neccessary

---
+++ addons/dvan/physician_search/components/PhysicianSearch.vue
@@ -0,0 +1,78 @@
+
+<script>
+import ToolTemplate from "/src/modules/tools/ToolTemplate.vue";
+import {mapGetters, mapActions, mapMutations} from "vuex";
+import getters from "../store/getters";
+
+export default {
+    name: "PhysicianSearch",
+    components: {
+        ToolTemplate
+    },
+    computed: {
+        ...mapGetters("Tools/PhysicianSearch", Object.keys(getters))
+    },
+    created () {
+        this.$on("close", this.close);
+    },
+    /**
+     * Put initialize here if mounting occurs after config parsing
+     * @returns {void}
+     */
+    mounted () {
+        this.initialize();
+        if (this.isActive) {
+            this.setActive(true);
+        }
+    },
+    methods: {
+        ...mapActions("Tools/PhysicianSearch", [
+            "initialize"
+        ]),
+        ...mapMutations("Tools/PhysicianSearch", [
+            "setActive"
+        ]),
+
+        /**
+         * Closes this tool window by setting active to false
+         * @returns {void}
+         */
+        close () {
+            this.setActive(false);
+
+            // TODO replace trigger when Menu is migrated
+            // set the backbone model to active false for changing CSS class in menu (menu/desktop/tool/view.toggleIsActiveClass)
+            // else the menu-entry for this tool is always highlighted
+            const model = Radio.request("ModelList", "getModelByAttributes", {id: this.$store.state.Tools.PhysicianSearch.id});
+
+            if (model) {
+                model.set("isActive", false);
+            }
+        }
+    }
+};
+</script>
+
+<template lang="html">
+    <ToolTemplate
+        :title="'Arztsuche'"
+        :icon="icon"
+        :active="active"
+        :render-to-window="renderToWindow"
+        :resizable-window="resizableWindow"
+        :deactivate-gfi="deactivateGFI"
+    >
+        <template #toolBody>
+            <div
+                v-if="active"
+                id="vue-addon"
+            >
+                {{ "Hier entsteht das Tool für die Arztsuche." }}
+            </div>
+        </template>
+    </ToolTemplate>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ addons/dvan/physician_search/store/state.js
@@ -0,0 +1,14 @@
+const state = {
+    // mandatory
+    active: false,
+    id: "PhysicianSearch",
+    // mandatory defaults for config.json parameters
+    name: "Arztsuche",
+    icon: "bi-pie-chart-fill",
+    renderToWindow: false,
+    resizableWindow: true,
+    isVisibleInMenu: true,
+    deactivateGFI: false
+};
+
+export default state;
