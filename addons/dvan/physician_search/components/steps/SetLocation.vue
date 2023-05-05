04628a8803bec54b254ef224f5bf9b7e2dfc7669
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-05-05 11:18:11 +0200

    Added new component for place search

---
+++ addons/dvan/physician_search/components/steps/SetLocation.vue
@@ -0,0 +1,31 @@
+<script>
+import {mapGetters, mapActions, mapMutations} from "vuex";
+import getters from "../../store/getters";
+
+export default {
+    name: "SetLocation",
+    components: {
+    },
+    data () {
+        return {
+        };
+    },
+    computed: {
+        ...mapGetters("Tools/SpatialAccess", Object.keys(getters))
+    },
+    methods: {
+        ...mapActions("Tools/SpatialAccess", [
+            "initialize"
+        ]),
+        ...mapMutations("Tools/SpatialAccess", [
+            "setActive"
+        ])
+    }
+};
+</script>
+
+<html lang="html">
+    <div>
+
+    </div>
+</html>
