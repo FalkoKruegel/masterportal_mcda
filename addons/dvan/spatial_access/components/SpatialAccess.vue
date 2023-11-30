3b6d66fb7207fbf287c32f3d01801767b14a16a6
ErikButhmann <erik.buthmann@mailbox.tu-dresden.de>
2023-11-30 12:50:06 +0100

    renamed id of spatial access accordion

--- addons/dvan/spatial_access/components/SpatialAccess.vue
+++ addons/dvan/spatial_access/components/SpatialAccess.vue
@@ -164,7 +164,7 @@ export default {
     >
         <template #toolBody>
             <div
-                id="decisionSupportAccordion"
+                id="spatialAccessAccordion"
                 class="accordion accordion-flush full-window"
             >
                 <AccordionItem
